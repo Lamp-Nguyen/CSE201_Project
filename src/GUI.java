2678627c09075fbe5c7cde0041e3906c0497da76
Lamp-Nguyen <lamnguyenmai02@gmail.com>
2022-04-07 12:05:46 -0400

    Converted to Maven project

--- src/GUI.java
+++ src/GUI.java
@@ -7,16 +7,10 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-<<<<<<< HEAD
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-=======
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
->>>>>>> d70c0b96a9804598892041d1f5fdf63bb90e0a15
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
@@ -24,51 +18,6 @@ import javax.swing.border.TitledBorder;
 
 public class GUI extends JFrame implements ActionListener {
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		frame.setSize(600, 700);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(panel);
-		panel.setLayout(null);
-		
-		//========================================== User name text and field
-		userName = new JLabel("Username: ");
-		userName.setBounds(150, 20, 80, 25);
-		panel.add(userName);
-		
-		userText = new JTextField(20);
-		userText.setBounds(230, 20, 165, 25);
-		panel.add(userText);
-		
-		//========================================== Password text and field
-		password = new JLabel("Password: ");
-		password.setBounds(150, 70, 80, 25);
-		panel.add(password);
-		
-		passText = new JPasswordField();
-		passText.setBounds(230, 70, 165, 25);
-		panel.add(passText);
-
-		//========================================== All data text field
-		display = new JTextArea();
-		display.setEditable(false);
-		scroll = new JScrollPane(display, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		display.setBounds(100, 260, 400, 280); 
-		scroll.setBounds(100, 260, 400, 280);
-
-		panel.add(scroll);
-    	
-		File file = new File("Mock_data.txt");
-		String line;
-		try{
-			Scanner sc = new Scanner(file);
-			while(sc.hasNext()){
-				line = sc.nextLine();
-				display.append(line + "\n");
-=======
 	private ArrayList<Business> catalogRecords = new ArrayList<Business>();
 	private ArrayList<Business> workingArr;
 	private JPanel mainPanel, functionPanel, userPanel, sortPanel, catalogPanel, catalogContainer;
@@ -252,7 +201,6 @@ public class GUI extends JFrame implements ActionListener {
 				catalogContainer.add(tmp);
 			} else {
 				loadBusinesses(workingArr, catalogContainer);
->>>>>>> d70c0b96a9804598892041d1f5fdf63bb90e0a15
 			}
 		} else if (e.getActionCommand().equals("Add Business")) {
 			AddBusinessFrame tmp = new AddBusinessFrame();
@@ -295,69 +243,6 @@ public class GUI extends JFrame implements ActionListener {
 		} finally {
 			cm.closeConnection();
 		}
-<<<<<<< HEAD
-
-		//========================================== Complete Label
-		complete = new JLabel("");
-		complete.setBounds(230, 150, 300, 25);
-		panel.add(complete);
-		
-		//========================================== Login button
-		login = new JButton("Login");
-		login.setBounds(230, 120, 80, 25);
-		login.addActionListener(new ActionListener() { 
-			public void actionPerformed(ActionEvent e) { 
-			  	String user = userText.getText();
-			  	String pass = passText.getText();
-
-			  	if(user.equals("abc") && pass.equals("123")){
-					complete.setText("Login succesful");
-			  	} else {
-				  	complete.setText("Login unsuccesful");
-			  	}
-			}	 
-		  } );
-		panel.add(login);
-		
-		//========================================== Clear button
-		clear = new JButton("Clear");
-		clear.setBounds(310, 120, 80, 25);
-		clear.addActionListener(new ActionListener() { 
-			public void actionPerformed(ActionEvent e) {
-				userText.setText("");
-				passText.setText("");
-				complete.setText("");
-			} 
-		  } );
-		panel.add(clear);
-
-		//========================================== Search Button
-		search = new JButton("Search");
-		search.setBounds(420, 190, 80, 25);
-		search.addActionListener(new ActionListener() { 
-			public void actionPerformed(ActionEvent e) {
-				String search = searchText.getText();
-
-			} 
-		  } );
-		panel.add(search);
-
-		//========================================== Categories and Search Box
-		String[] options = {"Category", "Name", "Date", "Rating", "Expense", "Type", "Owner", "Number"};
-		JComboBox<String> category = new JComboBox<>(options);
-		category.setBounds(100, 190, 110, 25);
-		panel.add(category);
-
-		searchText = new JTextField("Search");
-		searchText.setBounds(230, 189, 168, 25);
-		panel.add(searchText);
-
-
-
-
-
-		frame.setVisible(true);
-=======
 	}
 	
 	public ArrayList<Business> search(ArrayList<Business> arr, String searchStr) {
@@ -471,6 +356,5 @@ public class GUI extends JFrame implements ActionListener {
 		app.setLocationRelativeTo(null);
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		app.setVisible(true);
->>>>>>> d70c0b96a9804598892041d1f5fdf63bb90e0a15
 	}
 }
